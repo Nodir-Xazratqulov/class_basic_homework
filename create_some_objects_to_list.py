cb3a42386e7b200bef0f81011c9c1a6a96de7b25
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-06-14 11:18:10 +0500

    class-3

--- create_some_objects_to_list.py
+++ create_some_objects_to_list.py
@@ -1,7 +1,16 @@
 from create_one_attribute import Person
 
-#Create an object named "p1" whose name is "Anvar"
-#Create an object named "p2" whose name is "Shavkat"
-#Create an object named "p3" whose name is "Jasur"
+#"P1" nomli ob'ekt yarating, uning nomi "Anvar"
+#"P2" nomli ob'ekt yarating, uning nomi "Shavkat"
+#"P3" nomli ob'ekt yarating, uning nomi "Jasur"
 
-#Add these objects to the "persons" named list
+#Ushbu ob'ektlarni "shaxslar" ro'yxatiga qo'shing
+
+P1=Person('Anvar')
+P2=Person('Shavkat')
+P3=Person('Jasur')
+
+persons=[P1,P2,P3]
+print(P1.ism, P2.ism, P3.ism)
+
+
