975d12b8e4ebcfb2dea5b2ce291ab1dce8f3f5cc
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-06-14 11:20:55 +0500

    class-4

--- create_some_objects.py
+++ create_some_objects.py
@@ -1,4 +1,9 @@
 from create_one_attribute import Person
 
-#create an object named "p1" whose name is "Anvar"
-#create an object named "p2" whose name is "Shavkat"
+#"P1" nomli ob'ekt yarating, uning nomi "Anvar"
+#"p2" nomli ob'ekt yarating, uning nomi "Shavkat"
+
+p1=Person('Anvar')
+p2=Person('Shavkat')
+print(p1.ism)
+print(p2.ism)
